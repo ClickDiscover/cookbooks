bd5c2f5014ed849c919eb245fbaf4cd9ae67cb88
Ilya Novikov <il.novikov@gmail.com>
2015-07-09 13:04:28 +0800

    configure_nginx: disable force_unlink

--- configure_nginx/recipes/default.rb
+++ configure_nginx/recipes/default.rb
@@ -7,7 +7,6 @@ end
 
 # configure nginx
 template '/etc/nginx/nginx.conf' do
-  force_unlink true
   source 'nginx.conf.erb'
   owner 'root'
   group 'root'
@@ -16,7 +15,6 @@ template '/etc/nginx/nginx.conf' do
 end
 
 template '/etc/nginx/conf.d/site.conf' do
-  force_unlink true
   source 'site.conf.erb'
   owner 'root'
   group 'root'

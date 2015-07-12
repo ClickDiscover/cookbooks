5165ddb4aac0ac2811ba1b298496e9a15f206ddf
Ilya Novikov <il.novikov@gmail.com>
2015-07-12 21:09:56 +0800

    update chef_gem

--- route53/recipes/default.rb
+++ route53/recipes/default.rb
@@ -1,6 +1,6 @@
 #
-chef_gem "aws-sdk" do
-  compile_time false
+chef_gem 'aws-sdk' do
+  compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
   action :install
 end
 

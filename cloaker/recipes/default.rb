d7b6637acac42949bdda00dd285ecc8d8c682e49
Ilya Novikov <il.novikov@gmail.com>
2015-07-13 22:41:35 +0800

    ensure /home/ec2-user directory exists and has correct permissions

--- cloaker/recipes/default.rb
+++ cloaker/recipes/default.rb
@@ -2,6 +2,14 @@
 # reverse server's hostname, then replace first dash, then reverse result again
 domain = node[:hostname].reverse.sub('-', '.').reverse
 
+# ensure /home/ec2-user directory exists and has correct permissions
+directory '/home/ec2-user' do
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0711'
+  action :create
+end
+
 # ensure /home/ec2-user/www directory exists
 directory '/home/ec2-user/www' do
   owner 'ec2-user'

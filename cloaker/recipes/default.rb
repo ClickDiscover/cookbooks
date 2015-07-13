87b3dc44e1223575eb3d93f400f48fdd0b745257
Ilya Novikov <il.novikov@gmail.com>
2015-07-13 23:10:36 +0800

    cloaker: execute curl as ec2-user

--- cloaker/recipes/default.rb
+++ cloaker/recipes/default.rb
@@ -2,6 +2,12 @@
 # reverse server's hostname, then replace first dash, then reverse result again
 domain = node[:hostname].reverse.sub('-', '.').reverse
 
+# set required parameters
+id   = node['cloaker']['id']
+host = node['cloaker']['gencloaker_host']
+port = node['cloaker']['gencloaker_port']
+name = if node['cloaker']['name'].present? then node['cloaker']['name'] else domain end
+
 # ensure /home/ec2-user directory exists and has correct permissions
 directory '/home/ec2-user' do
   owner 'ec2-user'
@@ -20,10 +26,8 @@ end
 
 # download cloaker script
 execute 'deploy-cloaker' do
-  id   = node['cloaker']['id']
-  host = node['cloaker']['gencloaker_host']
-  port = node['cloaker']['gencloaker_port']
-  name = if node['cloaker']['name'].present? then node['cloaker']['name'] else domain end
+  user 'ec2-user'
+  group 'ec2-group'
 
   command <<-EOH
     curl http://#{host}:#{port}/cloaker?id=#{id}&name=#{name} > /home/ec2-user/www/index.php

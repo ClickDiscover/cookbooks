ee6104192cb2fb4d15ec4198413fc48812d1d9ea
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 23:05:45 +0800

    centrifuge: add config.php

---
+++ deploy/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:centrifuge][:ttl] = 3600
+default[:centrifuge][:env] = 'production'
+default[:centrifuge][:pdo_url] = 'pgsql:host=localhost;dbname=rotator;port=5432;user=rotator'
+default[:centrifuge][:log_level] = 'Monolog\Logger::INFO'
+default[:centrifuge][:fallback_lander] = 1
--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -12,3 +12,11 @@ node[:deploy].each do |application, deploy|
     app application
   end
 end
+
+# configure Centrifuge
+template '/srv/www/centrifuge/current/Centrifuge/config.php' do
+  source 'centrifuge.config.php.erb'
+  owner node[:opsworks][:deploy_user][:user]
+  group node[:opsworks][:deploy_user][:group]
+  mode '0640'
+end

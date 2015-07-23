6bed7914dc6f343d0696daeba8bcc3c9da164734
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 17:25:50 +0800

    general collectd cookbook cleanup

--- collectd/recipes/default.rb
+++ collectd/recipes/default.rb
@@ -1,26 +1,18 @@
-yum_package 'collectd' do
-  action :install
-end
+# install required packages
+['collectd', 'collectd-curl', 'collectd-curl_xml', 'collectd-nginx'].each {|x|
+  yum_package "#{x}" do
+    action :install
+  end
+}
 
-yum_package 'collectd-curl' do
-  action :install
-end
-
-yum_package 'collectd-curl_xml' do
-  action :install
-end
-
-yum_package 'collectd-nginx' do
-  action :install
-end
-
-# define nginx system service
+# define collectd system service
 service 'collectd' do
   service_name 'collectd'
   supports :restart => true, :status => true, :reload => true
   action [:enable, :start]
 end
 
+# configure collectd
 template '/etc/collectd.conf' do
   source 'collectd.conf.erb'
   owner 'root'
@@ -28,19 +20,3 @@ template '/etc/collectd.conf' do
   mode '0644'
   notifies :reload, 'service[collectd]', :immediately
 end
-
-# template '/etc/collectd.d/nginx.conf' do
-#   source 'collectd.conf.erb'
-#   owner 'root'
-#   group 'root'
-#   mode '0644'
-#   notifies :reload, 'service[collectd]', :immediately
-# end
-
-# template '/etc/collectd.conf' do
-#   source 'collectd.conf.erb'
-#   owner 'root'
-#   group 'root'
-#   mode '0644'
-#   notifies :reload, 'service[collectd]', :immediately
-# end

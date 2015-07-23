0c6b8f18fb3a3947ab978721ef27a120a1f7c136
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 21:55:33 +0800

    deploy: fix recipe

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -2,8 +2,8 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
+    user node[:opsworks][:deploy_user][:user]
+    group node[:opsworks][:deploy_user][:group]
     path deploy[:deploy_to]
   end
 

d4d71b0b4afea411db33b2455e3046d8df77fba7
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 22:05:11 +0800

    deploy: revert recipes/default.rb

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -2,8 +2,8 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   opsworks_deploy_dir do
-    user node[:opsworks][:deploy_user][:user]
-    group node[:opsworks][:deploy_user][:group]
+    user deploy[:user]
+    group deploy[:group]
     path deploy[:deploy_to]
   end
 

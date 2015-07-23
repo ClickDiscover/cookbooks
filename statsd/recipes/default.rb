0ff86b450679304c6ca664d9968cca2463302114
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 17:32:54 +0800

    statsd: general cleanup

--- statsd/recipes/default.rb
+++ statsd/recipes/default.rb
@@ -1,18 +1,18 @@
-
+# install statsd
 include_recipe "statsd::install"
 
 # configure the service
 include_recipe "statsd::configure"
 
-template '/etc/init.d/statsd' do
-  source 'initd.conf.erb'
-  mode 0644
-  notifies :restart, 'service[statsd]', :delayed
-end
-
+# define statsd service
 service 'statsd' do
   service_name 'statsd'
   supports :restart => true, :status => true, :reload => true
   action [:enable, :start]
 end
 
+template '/etc/init.d/statsd' do
+  source 'initd.conf.erb'
+  mode 0644
+  notifies :restart, 'service[statsd]', :delayed
+end

67525bbb15d55e8f7853cd15021e9cdbf0c6348b
Ilya Novikov <il.novikov@gmail.com>
2015-07-23 23:57:44 +0800

    deploy: fix config.php permissions

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -19,7 +19,7 @@ template '/srv/www/centrifuge/current/config.php' do
   source 'centrifuge.config.php.erb'
   owner node[:opsworks][:deploy_user][:user]
   group node[:opsworks][:deploy_user][:group]
-  mode '0640'
+  mode '0644'
 end
 
 # symlink static files

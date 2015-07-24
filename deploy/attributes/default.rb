e0a9f89d1f989e5f9ea1215d4b72f2d9d7e03e9b
Patrick Madden <maddenpj@gmail.com>
2015-07-23 23:39:56 -0500

    Add click method configs

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -3,4 +3,5 @@ default[:centrifuge][:env] = 'production'
 default[:centrifuge][:pdo_url] = 'pgsql:host=localhost;dbname=rotator;port=5432;user=rotator'
 default[:centrifuge][:log_level] = 'Monolog\Logger::INFO'
 default[:centrifuge][:fallback_lander] = 1
-default[:centrifuge][:click_url] = 'null'
+default[:centrifuge][:click_method] = 'redirect'
+default[:centrifuge][:click_url] = '"http://cpv.flagshippromotions.com/base2.php"'

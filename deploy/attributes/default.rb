c2528741da0af836bc6884caefc1a49dafeaa78c
Patrick Madden <maddenpj@gmail.com>
2015-07-23 21:51:34 -0500

    Add click url config

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -3,3 +3,4 @@ default[:centrifuge][:env] = 'production'
 default[:centrifuge][:pdo_url] = 'pgsql:host=localhost;dbname=rotator;port=5432;user=rotator'
 default[:centrifuge][:log_level] = 'Monolog\Logger::INFO'
 default[:centrifuge][:fallback_lander] = 1
+default[:centrifuge][:click_url] = 'null'

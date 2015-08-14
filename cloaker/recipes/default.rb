03e30d7a281914a5afae35aced01bc2be943f024
Ilya Novickov <il.novikov@gmail.com>
2015-08-14 14:32:14 +0800

    cloaker: don't proceed if cloaker is already installed and reinstall flag isn't set

--- cloaker/recipes/default.rb
+++ cloaker/recipes/default.rb
@@ -1,3 +1,12 @@
+# don't proceed if cloaker is already installed and reinstall flag isn't set
+if File.exists?("/home/#{user}/www/index.php") and !node['cloaker']['reinstall']
+  log 'message' do
+    message "******Skipping Cloaker index.php creation as it already exists and the 'reinstall' flag isn't set******"
+    level :info
+  end
+  return
+end
+
 # don't proceed if id or name aren't set
 if not node['cloaker']['id']
   raise "Unable to set up cloaker: id isn't set"

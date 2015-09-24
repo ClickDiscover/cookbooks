143949a1ba5c08a195e1b87b270da727ecde19b1
Ilya Novickov <il.novikov@gmail.com>
2015-09-24 22:36:30 +0800

    cloaker/recipes/setup.rb: use attributes instead of hardcoded values

--- cloaker/recipes/setup.rb
+++ cloaker/recipes/setup.rb
@@ -1,15 +1,8 @@
-# ensure /home/ec2-user directory exists and has correct permissions
-directory '/home/ec2-user' do
-  owner 'ec2-user'
-  group 'ec2-user'
-  mode '0711'
-  action :create
-end
-
-# ensure /home/ec2-user/www directory exists
-directory '/home/ec2-user/www' do
-  owner 'ec2-user'
-  group 'ec2-user'
+# ensure cloaker directory exists
+directory node['cloaker']['dir'] do
+  owner node['cloaker']['user']
+  group node['cloaker']['group']
   mode '0755'
   action :create
+  recursive true
 end

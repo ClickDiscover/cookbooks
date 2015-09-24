e2e6f884f450cd2513807ea9881538c0d66300f7
Ilya Novickov <il.novikov@gmail.com>
2015-09-24 21:02:55 +0800

    add [cloaker][url]

--- cloaker/attributes/default.rb
+++ cloaker/attributes/default.rb
@@ -1,3 +1,4 @@
 default['cloaker']['id'] = nil
+default['cloaker']['url'] = nil
 default['cloaker']['name'] = nil
 default['cloaker']['reinstall'] = false
--- cloaker/recipes/default.rb
+++ cloaker/recipes/default.rb
@@ -1,7 +1,11 @@
 user = 'ec2-user'
 group = 'ec2-user'
 
-index_path = "/home/#{user}/www/index.php"
+cloaker_dir = "/home/#{user}/www"
+index_path = "#{cloaker_dir}/index.php"
+
+# the directory to download ['cloaker']['url'] to
+wgetdir = '/tmp/website'
 
 # don't proceed if cloaker is already installed and reinstall flag isn't set
 if File.exists?(index_path) and !node['cloaker']['reinstall']
@@ -18,10 +22,36 @@ if not node['cloaker']['name']
   raise "Unable to set up cloaker: name isn't set"
 end
 
+# remove cloaker_dir (the web server root) if it already exists
+# so there is no stalled files while running as an ad-hoc command
+directory "#{cloaker_dir}" do
+  action :delete
+  recursive true
+end
+
 # set up cloaker script
 template index_path do
   source 'index.php.erb'
   owner user
   group group
   mode '0644'
+end
+
+if node['cloaker']['url']
+  # download URL
+  execute "/usr/bin/wget -mkEpnp -nH -P /tmp/website -e robots=off #{node['cloaker']['url']}"
+
+  # rename index.php if it exists in the downloaded data
+  if File.exist?("#{wgetdir}/index.php")
+    execute "/bin/mv #{wgetdir}/index.php #{wgetdir}/index.php.remote"
+  end
+
+  # copy contents of the tmp directory to web server root
+  execute "/usr/bin/rsync #{wgetdir}/ #{cloaker_dir}/"
+
+  # remove the tmp directory
+  directory "#{wgetdir}" do
+    action :delete
+    recursive true
+  end
 end

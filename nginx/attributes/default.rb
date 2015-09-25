b455f56fc70fa1e36801e24e2b6885a7ac6c0e36
Ilya Novickov <il.novikov@gmail.com>
2015-09-25 12:34:08 +0800

    nginx: add default u/p

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,2 +1,4 @@
 default['nginx']['root_dir'] = '/home/ec2-user/www'
 default['nginx']['htpasswd'] = '/etc/nginx/htpasswd'
+default['nginx']['admin_user'] = 'admin'
+default['nginx']['admin_password'] = '0verridem3'

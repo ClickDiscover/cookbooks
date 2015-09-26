df760aff68f4bf39c76b36db42ee59c6ebeae236
Patrick Madden <maddenpj@gmail.com>
2015-09-26 17:10:38 -0500

    Get dat badger

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,4 +1,4 @@
 default['nginx']['root_dir'] = '/home/ec2-user/www'
 default['nginx']['htpasswd'] = '/etc/nginx/htpasswd'
 default['nginx']['admin_user'] = 'admin'
-default['nginx']['admin_password'] = '0verridem3'
+default['nginx']['admin_password'] = 'Badgerlife17'

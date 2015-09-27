0ecc55db27cb2502f58db552772d3293b59f6a07
Ilya Novickov <il.novikov@gmail.com>
2015-09-27 21:01:15 +0800

    cloaker: update site_mirror recipe

--- cloaker/recipes/site_mirror.rb
+++ cloaker/recipes/site_mirror.rb
@@ -1,20 +1,39 @@
 if node['cloaker']['url']
+  # preserve index.php before data removal
+  if File.exist?("#{node['cloaker']['index']}")
+    execute "mv #{node['cloaker']['index']} /tmp/index.php"
+  end
+
+  # recreate cloaker directory
+  directory node['cloaker']['dir'] do
+    action :delete
+    recursive true
+  end
+  directory node['cloaker']['dir'] do
+    owner node['cloaker']['user']
+    group node['cloaker']['group']
+    mode '0755'
+    action :create
+    recursive true
+  end
+  execute "mv /tmp/index.php #{node['cloaker']['index']}"
+
   # download URL
-  execute "/usr/bin/wget --timeout=10 -mkEpnp -nH -q -P #{node['cloaker']['wgetdir']} -e robots=off #{node['cloaker']['url']} || /bin/true" do
+  execute "wget --timeout=10 -mkEpnp -nH -q -P #{node['cloaker']['wgetdir']} -e robots=off #{node['cloaker']['url']} || /bin/true" do
     user node['cloaker']['user']
     group node['cloaker']['group']
   end
 
-  # rename index.php if it exists in the downloaded data
+  # rename index.php if it exists in downloaded data
   if File.exist?("#{node['cloaker']['wgetdir']}/index.html")
-    execute "/bin/mv #{node['cloaker']['wgetdir']}/index.php #{node['cloaker']['wgetdir']}/#{node['cloaker']['mirror_fallback']}"
+    execute "mv #{node['cloaker']['wgetdir']}/index.php #{node['cloaker']['wgetdir']}/#{node['cloaker']['mirror_fallback']}"
   end
 
-  # copy contents of the tmp directory to web server root
-  execute "/usr/bin/rsync -a #{node['cloaker']['wgetdir']}/ #{node['cloaker']['dir']}/"
+  # copy contents of tmp directory to web server root
+  execute "rsync -a #{node['cloaker']['wgetdir']}/ #{node['cloaker']['dir']}/"
 
-  # remove the tmp directory
-  directory "#{node['cloaker']['wgetdir']}" do
+  # remove tmp directory
+  directory node['cloaker']['wgetdir'] do
     action :delete
     recursive true
   end

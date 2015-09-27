015a8c076cfc3f364ec907583f7627c7b560400c
Ilya Novickov <il.novikov@gmail.com>
2015-09-27 14:58:25 +0800

    update "file"

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -25,7 +25,7 @@ end
 execute "#{node['deploy']['chef_client']} --chef-zero-port 8890 -j #{deploy_json} -L #{deploy_setup_log} -c #{node['deploy']['stage2']} -o #{node['deploy']['stage2_cmd']}"
 
 # remove temporary json file
-file deploy_json do
+file "#{node['deploy']['json']}" do
   action :delete
 end
 

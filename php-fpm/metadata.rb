e9f83e8c4be8f0e7ab2b65e9e90fb6d0dd585ece
Ilya Novickov <il.novikov@gmail.com>
2015-09-29 12:17:12 +0800

    add aerospike to metadata

--- php-fpm/metadata.rb
+++ php-fpm/metadata.rb
@@ -6,6 +6,7 @@ description       'Configures PHP-FPM'
 version           '0.8'
 recipe            'php-fpm', 'Configures PHP-FPM'
 recipe            'php-fpm::collectd', 'Configures collectd for PHP-FPM'
+recipe            'php-fpm::aerospike', 'Installs and configures aerospike PHP module'
 depends           'collectd'
 
 # https://supermarket.chef.io/cookbooks/yum
